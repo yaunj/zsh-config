d4c23d4948e936c8bc4cc1abd4bf804e65506b5d
Rune Hammersland <rune@hammersland.net>
2012-01-13 01:59:25 +0100

    Fixed wrong usage of test.

--- install.sh
+++ install.sh
@@ -6,18 +6,18 @@ for f in *~($(basename $0)|README*); do
     link=${HOME}/.${f}
     this=${PWD}/${f}
 
-    if [[ -e "${link}" ]]; then
-        if [[ -L "${link}" ]]; then
-            end=$(python -c "import os; print os.path.realpath('${link}');")
+    if [[ -L "${link}" ]]; then
+        end=$(python -c "import os; print os.path.realpath('${link}');")
 
-            if [[ "${end}" != "${this}" ]]; then
-                echo "[FAIL] ${link} points elsewhere. Fix it yourself." >&2
-            fi
-        else
-            echo "[FAIL] .${f} exists in \$HOME. Symlink it yourself." >&2
+        if [[ "${end}" != "${this}" ]]; then
+            echo "[FAIL] ${link} points elsewhere. Fix it yourself." >&2
         fi
     else
-        ln -s "${this}" "${link}"
-        echo "[ OK ] Created $link" >&2
+        if [[ -f "${link}" -o -d "${link}" ]]; then
+            echo "[FAIL] .${f} exists in \$HOME. Symlink it yourself." >&2
+        else
+            ln -s "${this}" "${link}"
+            echo "[ OK ] Created $link" >&2
+        fi
     fi
 done
